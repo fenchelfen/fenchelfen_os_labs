b08873f44087e3b230b25127a386c5fca780b145
fenchelfen <fenchel.fen@gmail.com>
2018-09-15 01:27:25 +0300

    Finish week 4 exercise 4
    Instead of using execve as was suggested in the course materials, I used
    execl. My exec function implementation fully copies the principles used in
    the sources of system function (stdlib.h).

---
+++ week4/ex4.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+#define BUFFER_MAX	32 
+#define CMD_START	"$ ) "
+
+const char **environ;
+char buffer[BUFFER_MAX];
+
+void *strip(char *string)
+{
+	const char *pattern = "\n\t";
+	char *byte = string;
+	while ((byte = strpbrk(byte, pattern))) {
+        strcpy(byte, byte+1);
+		++byte;
+	}
+}
+
+char **split(const char *string)
+{
+	const char delim = ' ';
+	int res_length = 0;
+	char *bytes = strdup(string);
+	char *byte = bytes;
+	while ((byte = strpbrk(byte, &delim))) {
+		++res_length;
+		++byte;
+	}
+	char **result = malloc((res_length+2)*sizeof(char*));
+	char *token;
+	int i = 0;
+	while ((token = strsep(&bytes, &delim))) {
+		result[i++] = strdup(token);
+	}
+	result[i] = NULL;
+	free(bytes);
+	return result;
+}
+
+void exec(char **argv)
+{
+    char **byte = argv;
+    char *command = malloc(sizeof(char*));
+    while (*byte) {
+        strcpy(&command[strlen(command)], *byte);
+        strcpy(&command[strlen(command)], " ");
+        ++byte;
+    }
+    pid_t childPid;
+    int status;
+    switch (childPid = fork()) {
+        case -1:
+            break;
+        case 0:
+            execl("/bin/sh", "sh", "-c", command, (char *) 0);
+            _exit(127);
+        default:
+            while (waitpid(childPid, &status, 0) == -1) {
+                if (errno != EINTR)
+                    break;
+            }
+        break;
+    }
+    byte = argv;
+	while (*byte)
+        free(*byte++);	
+    free(argv); // double-free?
+}
+
+int main(int argc, char **argv, char **arvp)
+{
+	fputs("Simplistic shell EXTENDED. Welcome\n\n", stdout);
+	int i = 0;
+	char c;
+	fputs(CMD_START, stdout);
+	while (fread(&c, sizeof(char), 1, stdin)) {
+		buffer[i] = c;
+		++i;
+		if (c == '\n') {
+			fputs(CMD_START, stdout);
+			if (i != 1) {
+				buffer[i] = '\0';
+                strip(buffer);
+				char **strings = split(buffer);
+				exec(strings);
+			}
+			i = 0;
+		}
+	}
+	fputs("\n\nSee you Space Cowboy\n", stdout);
+	return 0;
+}
