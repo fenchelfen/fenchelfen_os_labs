7be593bcc96e74a24494ab566f8e89265b07bee2
fenchelfen <fenchel.fen@gmail.com>
2018-09-15 12:13:59 +0300

    Fix tabspaces in ex 4, w 4

--- week4/ex4.c
+++ week4/ex4.c
@@ -5,41 +5,41 @@
 #include <errno.h>
 #include <sys/wait.h>
 
-#define BUFFER_MAX	32 
-#define CMD_START	"$ ) "
+#define BUFFER_MAX  32 
+#define CMD_START   "$ ) "
 
 const char **environ;
 char buffer[BUFFER_MAX];
 
 void *strip(char *string)
 {
-	const char *pattern = "\n\t";
-	char *byte = string;
-	while ((byte = strpbrk(byte, pattern))) {
+    const char *pattern = "\n\t";
+    char *byte = string;
+    while ((byte = strpbrk(byte, pattern))) {
         strcpy(byte, byte+1);
-		++byte;
-	}
+        ++byte;
+    }
 }
 
 char **split(const char *string)
 {
-	const char delim = ' ';
-	int res_length = 0;
-	char *bytes = strdup(string);
-	char *byte = bytes;
-	while ((byte = strpbrk(byte, &delim))) {
-		++res_length;
-		++byte;
-	}
-	char **result = malloc((res_length+2)*sizeof(char*));
-	char *token;
-	int i = 0;
-	while ((token = strsep(&bytes, &delim))) {
-		result[i++] = strdup(token);
-	}
-	result[i] = NULL;
-	free(bytes);
-	return result;
+    const char delim = ' ';
+    int res_length = 0;
+    char *bytes = strdup(string);
+    char *byte = bytes;
+    while ((byte = strpbrk(byte, &delim))) {
+        ++res_length;
+        ++byte;
+    }
+    char **result = malloc((res_length+2)*sizeof(char*));
+    char *token;
+    int i = 0;
+    while ((token = strsep(&bytes, &delim))) {
+        result[i++] = strdup(token);
+    }
+    result[i] = NULL;
+    free(bytes);
+    return result;
 }
 
 void exec(char **argv)
@@ -63,35 +63,35 @@ void exec(char **argv)
             while (waitpid(childPid, &status, 0) == -1) {
                 if (errno != EINTR)
                     break;
-            }
-        break;
-    }
+                }
+            break;
+        }
     byte = argv;
-	while (*byte)
-        free(*byte++);	
+    while (*byte)
+    free(*byte++);  
     free(argv); // double-free?
 }
 
-int main(int argc, char **argv, char **arvp)
+int main(int argc, char **argv, char **envp)
 {
-	fputs("Simplistic shell EXTENDED. Welcome\n\n", stdout);
-	int i = 0;
-	char c;
-	fputs(CMD_START, stdout);
-	while (fread(&c, sizeof(char), 1, stdin)) {
-		buffer[i] = c;
-		++i;
-		if (c == '\n') {
-			fputs(CMD_START, stdout);
-			if (i != 1) {
-				buffer[i] = '\0';
+    fputs("Simplistic shell EXTENDED. Welcome\n\n", stdout);
+    int i = 0;
+    char c;
+    fputs(CMD_START, stdout);
+    while (fread(&c, sizeof(char), 1, stdin)) {
+        buffer[i] = c;
+        ++i;
+        if (c == '\n') {
+            fputs(CMD_START, stdout);
+            if (i != 1) {
+                buffer[i] = '\0';
                 strip(buffer);
-				char **strings = split(buffer);
-				exec(strings);
-			}
-			i = 0;
-		}
-	}
-	fputs("\n\nSee you Space Cowboy\n", stdout);
-	return 0;
+                char **strings = split(buffer);
+                exec(strings);
+            }
+            i = 0;
+        }
+    }
+    fputs("\n\nSee you Space Cowboy\n", stdout);
+    return 0;
 }
